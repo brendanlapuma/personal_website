a53037993aa14d32ce33bc80f506c766c7c84f40
brendanlapuma <brendanlapuma@gmail.com>
2023-11-03 18:50:07 -0400

    fixed hero resize issue

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -12,10 +12,11 @@ import {
 } from "./HeroElements";
 import { TypeAnimation } from 'react-type-animation';
 import ScrollAnimation from "react-animate-on-scroll";
+import Grid from '@mui/material/Unstable_Grid2';
 
 function Hero() {
   const [isOpen, setIsOpen] = useState(false);
-  const [showSubtitle, setShowSubtitle] = useState(false);
+  //const [showSubtitle, setShowSubtitle] = useState(false);
   const [showScrollDown, setShowScrollDown] = useState(false);
 
   const toggle = () => {
@@ -25,87 +26,90 @@ function Hero() {
     <main>
       <Dropdown isOpen={isOpen} toggle={toggle} />
       <Header toggle={toggle} />
-      <HeroContainer>
-        <HeroWrapper>
-          <HeroLeft>
-            <ScrollAnimation animateIn="fadeIn" >
-              <TypeAnimation
-                cursor={false}
-                sequence={[
-                  'Hi, I\'m Brendan.',
-                  () => setShowSubtitle(true)
-                ]}
-                speed={{ type: "keyStrokeDelayInMs", value: 150 }}
-                wrapper="h1"
-                repeat={0}
-              />
-              {showSubtitle &&
-                <TypeAnimation
-                  cursor={true}
-                  sequence={[
-                    500,
-                    'A Full-Stack Developer and Data Scientist.',
-                    1000,
-                    'An undergraduate at the University of Maryland - College Park.',
-                    // 'I design and code beautifully simple things, and I love what I do.',
-                    1000,
-                    'A problem solver.',
-                    1000,
-                    'An innovative thinker.',
-                    1000,
-                    'A....',
-                    1000,
-                    'A.... cool guy?',
-                    1000,
-                    "Ok...",
-                    1000,
-                    "Ok...  I'm running out of ideas...",
-                    1000,
-                    "Uhh...",
-                    1000,
-                    "Hello World?",
-                    1000,
-                    "Uhh... you can scroll down to see my projects now...",
-                    300,
-                    () => setShowScrollDown(true),
-                    1000,
-                    "Seriously, my projects are really cool, go check them out!",
-                    1000,
-                    "You're uh...",
-                    1000,
-                    "You're uh... still here?",
-                    1000,
-                    "Ok, this has been fun, but I'm gonna restart the loop now...",
-                    // 1000,
-                    // "Or...",
-                    // 1000,
-                    // "Or... I could scroll you by force! Muahaha!",
-                    1000,
-                    "See ya! :)",
-                    500,
-                  ]}
-                  speed={50}
-                  deletionSpeed={65}
-                  wrapper="h5"
-                  repeat={Infinity}
-                />
-              }
-            </ScrollAnimation>
+          <HeroContainer>
+            <HeroWrapper>
+              <HeroLeft>
+                <ScrollAnimation animateIn="fadeIn" >
+                  <TypeAnimation
+                    cursor={false}
+                    sequence={[
+                      'Hi, I\'m Brendan.',
+                      //() => setShowSubtitle(true)
+                    ]}
+                    speed={{ type: "keyStrokeDelayInMs", value: 150 }}
+                    wrapper="h1"
+                    repeat={0}
+                  />
+                  {//showSubtitle &&
+                    <TypeAnimation
+                      cursor={true}
+                      sequence={[
+                        3000,
+                        '',
+                        700,
+                        'A Full-Stack Developer and Data Scientist.',
+                        1200,
+                        'An undergraduate at the University of Maryland - College Park.',
+                        // 'I design and code beautifully simple things, and I love what I do.',
+                        1200,
+                        'A problem solver.',
+                        1200,
+                        'An innovative thinker.',
+                        1000,
+                        'A....',
+                        1000,
+                        'A.... cool guy?',
+                        1000,
+                        "Ok...",
+                        1000,
+                        "Ok...  I'm running out of ideas...",
+                        1000,
+                        "Uhh...",
+                        1000,
+                        "Hello World?",
+                        1200,
+                        "Uhh... you can scroll down to see my projects now...",
+                        300,
+                        () => setShowScrollDown(true),
+                        1200,
+                        "Seriously, my projects are really cool, go check them out!",
+                        1000,
+                        "You're uh...",
+                        1000,
+                        "You're uh... still here?",
+                        1200,
+                        "Ok, this has been fun, but I'm gonna restart the loop now...",
+                        // 1000,
+                        // "Or...",
+                        // 1000,
+                        // "Or... I could scroll you by force! Muahaha!",
+                        1000,
+                        "See ya! :)",
+                        500,
+                      ]}
+                      speed={50}
+                      deletionSpeed={65}
+                      wrapper="h5"
+                      repeat={Infinity}
+                    />
+                  }
+                  
+                </ScrollAnimation>
 
-          </HeroLeft>
-        </HeroWrapper>
-        {showScrollDown &&<ScrollAnimation animateIn="flipInX" offset={0}>
-        <ScrollDown to="projects" id="scrollDown">
-          <ScrollLink>
-            Scroll down
-            <img
-              src="/scroll-down.svg"
-              alt="scroll-down"
-            />
-          </ScrollLink>
-        </ScrollDown>
-        </ScrollAnimation>}
-      </HeroContainer>
+              </HeroLeft>
+            </HeroWrapper>
+            {showScrollDown &&<ScrollAnimation animateIn="flipInX" offset={0}>
+            <ScrollDown to="projects" id="scrollDown">
+              <ScrollLink>
+                Scroll down
+                <img
+                  src="/scroll-down.svg"
+                  alt="scroll-down"
+                />
+              </ScrollLink>
+            </ScrollDown>
+            </ScrollAnimation>}
+          </HeroContainer>
     </main>
   );
 }
